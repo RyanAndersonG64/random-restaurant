4ff4dca83be7b65535d08d0c0b2656e0ca2033b2
RyanAndersonG64 <ryan.anderson.g64@gmail.com>
2024-05-23 11:55:21 -0400

    added order screen

--- src/OrderScreen.jsx
+++ src/OrderScreen.jsx
@@ -5,3 +5,10 @@ import SelectedMenu from "./SelectedMenu"
 
 // button to place an order
 // customer can select from local storage list of names and addresses, or enter new name and address
+
+function OrderScreen() {
+    return(
+        <div>hue</div>
+    )
+}
+export default OrderScreen
